6eb357509655e1bbbdec6d94055350999bd3baa7
Great_Monguss <kimmo1028@gmail.com>
2025-12-11 01:16:37 +0900

    [level 2] Title: 타겟 넘버, Time: 168.73 ms, Memory: 48.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,10 @@
+def solution(numbers, target):
+    answer = 0
+    leaves = [0]
+    for num in numbers:
+        tmp = []
+        for parent in leaves:
+            tmp.append(parent + num)
+            tmp.append(parent - num)
+        leaves = tmp
+    return leaves.count(target)
