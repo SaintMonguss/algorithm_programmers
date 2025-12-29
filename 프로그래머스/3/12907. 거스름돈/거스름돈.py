4caf20a2dbb62984f437595a977a8aa310e24b47
Great_Monguss <kimmo1028@gmail.com>
2025-12-30 00:20:51 +0900

    [level 3] Title: 거스름돈, Time: 488.65 ms, Memory: 12.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,9 @@
+
+def solution(n, money):
+    dp = [1] + [0] * n
+    for coin in money:
+        for i in range(coin, n + 1):
+            if i >= coin:
+                dp[i] += dp[i - coin]
+    
+    return dp[n] % 1000000007
